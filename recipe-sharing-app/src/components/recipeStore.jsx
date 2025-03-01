00be2725fb999053c450595a4655ea0f46cbbb3e
Abdul-Rahman <bodysttar@gmail.com>
2025-03-01 17:42:00 +0200

    Eighth Commit.

---
+++ recipe-sharing-app/src/components/recipeStore.jsx
@@ -0,0 +1,8 @@
+/*eslint-disable */
+import create from 'zustand'
+
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
